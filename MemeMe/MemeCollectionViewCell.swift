146b7047f2c322cc376b1e62ceb8253e99b296db
Kelly Egan <kelly.egan@gmail.com>
2015-06-09 15:53:53 -0400

    Add MemeCollectionViewCell

---
+++ MemeMe/MemeCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MemeCollectionViewCell.swift
+//  MemeMe
+//
+//  Created by Kelly Egan on 6/9/15.
+//  Copyright (c) 2015 Kelly Egan. All rights reserved.
+//
+
+import UIKit
+
+class MemeCollectionViewCell: UICollectionViewCell {
+    
+}
