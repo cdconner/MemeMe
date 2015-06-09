45b6de251b5ab70161f5015cda21212ec6150974
Kelly Egan <kelly.egan@gmail.com>
2015-06-09 19:36:55 -0400

    Add UITableViewDataSource and UITableViewDelegate to MemeTableController

--- MemeMe/MemeTableController.swift
+++ MemeMe/MemeTableController.swift
@@ -8,12 +8,20 @@
 
 import UIKit
 
-class MemeTableController: UIViewController {
+class MemeTableController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCellWithIdentifier("MemeTableCell", forIndexPath: indexPath) as! UITableViewCell
+        
+        return cell
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
     }
 
 }
