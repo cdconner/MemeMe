76a7390d56974ea833b209d4e0fc8fe4c410c33f
Kelly Egan <kelly.egan@gmail.com>
2015-06-11 22:06:18 -0400

    Add memes data to tableView

--- MemeMe/MemeTableController.swift
+++ MemeMe/MemeTableController.swift
@@ -10,19 +10,41 @@ import UIKit
 
 class MemeTableController: UIViewController, UITableViewDataSource, UITableViewDelegate {
     
+    var memes: [Meme]!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
+        super.viewDidLoad()
+        
         let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
         if( appDelegate.memes.count == 0 ) {
             launchMemeEditor()
         }
     }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        memes = appDelegate.memes
+        
+        //If you don't reload the table it won't see any changes!
+        self.tableView.reloadData()
+    }
+    
+    
 
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return self.memes.count
     }
     
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         var cell = tableView.dequeueReusableCellWithIdentifier("MemeTableCell", forIndexPath: indexPath) as! UITableViewCell
+        
+        let meme = memes[indexPath.row]
+        
+        cell.textLabel?.text = "\(meme.topText)..."
+        cell.imageView?.image = meme.memedImage
         
         return cell
     }
