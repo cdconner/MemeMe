e084694459beec195e5414b26cd96109eb7345f5
Kelly Egan <kelly.egan@gmail.com>
2015-06-12 16:01:06 -0400

    Implement collection view. Add UICollectionViewDatasource to MemeCollectionController. No detail or new Meme functionality yet.

--- MemeMe/MemeCollectionController.swift
+++ MemeMe/MemeCollectionController.swift
@@ -8,12 +8,33 @@
 
 import UIKit
 
-class MemeCollectionController: UIViewController {
+class MemeCollectionController: UIViewController, UICollectionViewDataSource {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    var memes: [Meme]!
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+        memes = appDelegate.memes
+        
+        //If you don't reload the table it won't see any changes!
+        self.collectionView.reloadData()
+    }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return memes.count
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("MemeCollectionViewCell", forIndexPath: indexPath) as! MemeCollectionViewCell
+        
+        let meme = memes[indexPath.row]
+        
+        cell.memeImage.image = meme.memedImage
+        
+        return cell
     }
     
 }
